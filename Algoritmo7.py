5463addb64efefb72812fc1502a00dc9900fbb19
CristianDavidFajardoRojas <160773269+CristianDavidFajardoRojas@users.noreply.github.com>
2024-02-23 13:56:56 -0500

    Ejercicio7

---
+++ Algoritmo7.py
@@ -0,0 +1,5 @@
+base = float(input("Escriba la base de el triangulo en metros: "))
+altura = float(input("Escriba la altura de el triangulo en metros: "))
+area = (base * altura)*1/2
+print("El area de el triangulo es ",area,"metros cuadrados")
+
