d24d0585b73d20ccbefc3dd59b7cf647fc0224f5
CristianDavidFajardoojas <160773269+CristianDavidFajardoRojas@users.noreply.github.com>
2024-02-23 10:40:25 -0500

    Ejercicio2

---
+++ Algoritmo2.py
@@ -0,0 +1,7 @@
+num1 = int(input("Escriba el primer número: "))
+num2 = int(input("Escriba el segundo número: "))
+num3 = int(input("Escriba el tercer número: "))
+mayor = max(num1, num2, num3)
+print("El número mayor es: ", mayor)
+
+    
