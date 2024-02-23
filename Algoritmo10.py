1db55b3d48efcb34ed794fd3934c819029d09309
CristianDavidFajardoRojas <160773269+CristianDavidFajardoRojas@users.noreply.github.com>
2024-02-23 14:56:06 -0500

    Ejercicio 9 y 10

---
+++ Algoritmo10.py
@@ -0,0 +1,7 @@
+num1 = int(input("Escriba el primer numero: "))
+num2 = int(input("Escriba el segundo numero: "))
+num3 = int(input("Escriba el tercer numero: "))
+num4 = int(input("Escriba el cuarto numero: "))
+num5 = int(input("Escriba el quinto numero: "))
+promedio=(num1 + num2 + num3 + num4 + num5)/5
+print("El promedio de la lista de 5 numeros es ",promedio)
