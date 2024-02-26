8d334eb45af97fcedd40995ac7946782ce76e6a5
CristianDavidFajardoRojas <160773269+CristianDavidFajardoRojas@users.noreply.github.com>
2024-02-26 13:15:52 -0500

    Ejercicios 8 y 9 ( resueltos )

---
+++ Algoritmo08.py
@@ -0,0 +1,10 @@
+num1 = int(input("Ingrese su primer número: "))
+num2 = int(input("ingrese su segundo número: "))
+num3 = num1 + num2
+num4 = num3 + num2
+num5 = num3 + num4
+num6 = num4 + num5
+num7 = num5 + num6
+
+print("la secuencia de fibonacci con la dos números ingresados es: ", num1, num2, num3, num4, num5, num6, num7, "...")
+
---
+++ Algoritmo09.py
@@ -0,0 +1,7 @@
+numero = int(input("Ingrese el número que desea: "))
+print("La tabla de multiplicar del ",numero, "es: ")
+for i in range(10):
+    resultado = numero * i
+    print(numero, " X ", i, "=", resultado)
+
+
